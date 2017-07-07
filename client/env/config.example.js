24c975a49f775ece1af75d134cfcbac45e08eecd
Hack Reactor Student <student@hackreactor.com>
2017-07-07 11:18:11 -0700

    API key and App ID key inserted

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,6 +3,6 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', '676a1e781a66c09718a32a0221ea3a23a9080272');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', '848f43ff2d1f6d372a9779c6be185d11f18cc183');
 });
