b88e8453ee284b643540dbaea368b0f333e89d5e
Hack Reactor Student <student@hackreactor.com>
2017-07-07 12:35:12 -0700

    display new message with GET request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,26 +1,44 @@
 // YOUR CODE HERE:
 
+$(document).ready(function() {
+  
+  let $body = $('body');
+  let $text;
 
-// http://parse.la.hackreactor.com/chatterbox/classes/messages
+  $('.get').on('click', function(event) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      // data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        // debugger;
+        $text = data.results[1].text;
+        //$('<div class="message"></div>').appendTo($body);
+        console.log($text);
+        $('<div>' + $text + '</div>').appendTo($body.find('#chats'));
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+    
+  });
 
 
-var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};
+  var message = {
+    username: 'shawndrost',
+    text: 'trololo',
+    roomname: '4chan'
+  };
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
+  
 });
+
+
+
+
+
+
