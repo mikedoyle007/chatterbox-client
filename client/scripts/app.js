398c20a4a59b27a89a01e6a4c3786d7655bee3d0
Hack Reactor Student <student@hackreactor.com>
2017-07-07 14:59:35 -0700

    Implemented fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,7 @@ let $body = $('body');
 
 let App = function () {
   this.etc;
+  this.server = 'http://parse.la.hackreactor.com/chatterbox/classes/messages';
 };
 
 let app = new App();
@@ -12,13 +13,12 @@ App.prototype.init = function () {
   //
 };
 
-
 App.prototype.send = function (message) {
 
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -34,16 +34,17 @@ App.prototype.send = function (message) {
 
 App.prototype.fetch = function () {
   let $text;
+  let $body = $('body'); 
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'GET',
     // data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       // debugger;
-      $text = data.results[1].text;
+      $text = data.results[0].text;
       //$('<div class="message"></div>').appendTo($body);
       console.log($text);
       $('<div>' + $text + '</div>').appendTo($body.find('#chats'));
@@ -62,16 +63,15 @@ $(document).ready(function() {
   
   
 
-  $('.get').on('click', app.fetch());
+  $('.get').on('click', app.fetch);
 
   
   $('.send').on('click', function() {
     var message = {
       username: 'shawndrost',
-      text: 'trololo',
+      text: $('#messageBox').val(),
       roomname: '4chan'
     };
-  
 
     app.send(message);
     console.log('sent! ', message);
