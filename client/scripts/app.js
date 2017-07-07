7b4faeeec53cc0d6929a644db71ae8053e2048e4
Hack Reactor Student <student@hackreactor.com>
2017-07-07 11:29:54 -0700

    updated dependencies bower and underscore

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,26 @@
 // YOUR CODE HERE:
+
+
+// http://parse.la.hackreactor.com/chatterbox/classes/messages
+
+
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
